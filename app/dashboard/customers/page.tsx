b0af63a43f0c2fc1899bdf41b885f707a844c162
Jefersonsteven <gfreycode@gmail.com>
2023-11-05 17:27:58 -0500

    Add metadata to pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,10 @@
 import { fetchCustomers } from '@/app/lib/data';
 import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Custormers',
+  };
 
 export default async function Page() {
     const customers = await fetchCustomers();
